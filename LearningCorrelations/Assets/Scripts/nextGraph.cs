39ce12081ca55be0bd8ecf78ff97d863450a9019
ShainaGlass <32582433+ShainaGlass@users.noreply.github.com>
2019-08-18 12:14:03 -0400

    Load next graph
    changed nextGraph scripts so when next graph is generated score remains and other variables are updated. One issue: old points are not being deleted.

--- LearningCorrelations/Assets/Scripts/nextGraph.cs
+++ LearningCorrelations/Assets/Scripts/nextGraph.cs
@@ -5,9 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class nextGraph : MonoBehaviour
 {
+    private Graph newGraph;
+    private GameplayManager gameplayManager;
 
     public void next() {
-        SceneManager.LoadScene("correlationGame");
+        //references to other scripts
+        newGraph = GameObject.FindObjectOfType<Graph>();
+        gameplayManager = GameObject.FindObjectOfType<GameplayManager>();
+
+        //destroy old points
+        Destroy(newGraph.pointSprite);
+
+        //get num of points on graph
+        int points = newGraph.pointNum;
+
+        //create new graph
+        newGraph.showGraph(points);
+
+        List<int> x = newGraph.x;
+        List<int> y = newGraph.y;
+
+        gameplayManager.updateCorrelation(x, y);
+
+        gameplayManager.nextButton.SetActive(false);
+
+        //reset text boxes
+        gameplayManager.showDiff.text = "Difference: ";
+        gameplayManager.showCorr.text = "Pearson Correlation: ";
+        gameplayManager.showText.text = "Your Guess: ";
+        
     }
 
 }
