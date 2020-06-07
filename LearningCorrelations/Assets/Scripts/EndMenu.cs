6ddf8268c759aa5bd05dbb524f6ca7e9761e6a5e
ShainaGlass <32582433+ShainaGlass@users.noreply.github.com>
2020-06-07 11:26:36 -0400

    saving code
    wrote simple code to test saing data in csv

--- LearningCorrelations/Assets/Scripts/EndMenu.cs
+++ LearningCorrelations/Assets/Scripts/EndMenu.cs
@@ -2,17 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 public class EndMenu : MonoBehaviour
 {
     [SerializeField] private Text scoreTxt;
     [SerializeField] private Text idTxt;
+    string path;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         //display end information
         idTxt.text = "ID: " + PlayerPrefs.GetString("participantID");
-        scoreTxt.text = "Score: " + PlayerPrefs.GetInt("pScore").ToString();   
+        scoreTxt.text = "Score: " + PlayerPrefs.GetInt("pScore").ToString();  
+        
+        path = "Assets/Files/" + PlayerPrefs.GetString("participantID") + ".csv";
+
+        saveData(path, PlayerPrefs.GetString("participantID"), PlayerPrefs.GetInt("pScore").ToString());
+    }
+
+    //test data saving
+    public static void saveData(string path, string pID, string pscore) {
+        using (System.IO.StreamWriter file = new System.IO.StreamWriter(path, true))
+        {
+            file.WriteLine(pID + "," + pscore);
+        }
     }
 }
